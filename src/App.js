ec264a5176846edbbf83b58f46c77da655f614cc
Kelvin Almeida <kelvin.f.almeida@gmail.com>
2019-04-02 16:41:08 -0300

    desenvolvendo serviços

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
+
+import Service from './components/Service.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p className="BoxText">Portfolio</p>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>*Bem na Web*</h1>
-        </header>
-      </div>
+      <Service/>
     );
   }
 }
