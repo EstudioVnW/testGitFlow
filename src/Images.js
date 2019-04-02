db90f8c63d47b8f5acda60a3b7ee8992a55c19b7
pedrocosta02 <henriquegamesrj3@gmail.com>
2019-04-02 13:59:32 -0300

    mexendo com componemtes

---
+++ src/Images.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Images extends Component {
+  render() {
+    return (
+      <div className="imageBox">
+        <div>
+            {/* <img src="Icone-Landing.svg"></img> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Images;
