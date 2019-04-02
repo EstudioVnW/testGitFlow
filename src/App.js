82fa98cbac9fdce88ed068560c88fd24f4ece1d8
pedrocosta02 <henriquegamesrj3@gmail.com>
2019-04-02 15:28:35 -0300

    clonando de novo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Images from './Images';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Bem na Web</h1>
-        </header>
+      <div className="servicosBox">
+        <div className="servicosBos_box-items">
+          <h1>Serviços</h1>
+          <p>Utilizamos as melhores plataformas e linguagens para entregar 
+            soluções digitais otimizadas, confiáveis e que 
+            atendam seus reais objetivos de negócios.</p>
+        </div>
+        <Images />
       </div>
     );
   }
